e9b03b98685063287fb1d7a4b3df3a279055402c
gnangndong <gnangndong@gaming.tech>
2023-11-08 15:27:02 +0100

    Collision réussi.

--- GameObject.h
+++ GameObject.h
@@ -5,18 +5,17 @@ class GameObject
 {
 public:
 	GameObject(float m_x, float m_y, float m_width, float m_height, float m_angle, float m_speed);
-	GameObject(float m_x, float m_y, float m_radius);
+	GameObject(float m_x, float m_y, float m_radius, float m_speed);
 	~GameObject();
 
 	void setPosition(float m_x, float m_y);
 	void setSize(int m_width, int m_height);
-	void setShapeRect();
-	void setShapeCircle();
 	void setRadius(float m_radius);
 	void setAngle(float m_angle);
 	void setRotation(float m_angle);
 	void setOrigin(float m_x, float m_y);
-	void move(float m_x, float m_y,float deltaTime, float speed);
+	void move(float deltaTime);
+	void collision(GameObject& other);
 	const sf::Shape& getShape();
 
 private:
--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
-#include <SFML/Graphics.hpp>
 #include "GameObject.h"
 
 int main() {
     sf::Clock clock;
-    float deltaTime = 0.0f;
+    float deltaTime = 0;
     sf::RenderWindow window(sf::VideoMode(800, 600), "GameObject Test");
     window.setFramerateLimit(60);
 
-    GameObject Rect(100.f, 100.f, 50, 50, 135.f,5);
-    GameObject Circle(200.f, 200.f, 100);
+    GameObject Rect(100.f, 100.f, 50, 50, 0.f,75.f);
+    GameObject Circle(200.f, 200.f, 100,20.f);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -17,10 +16,16 @@ int main() {
             }
         }
 
+        Rect.collision(Circle);
+        Circle.collision(Rect);
+        Rect.move(deltaTime);
+        Circle.move(deltaTime);
+
         window.clear();
         window.draw(Circle.getShape());
         window.draw(Rect.getShape());
         window.display();
+        deltaTime = clock.restart().asSeconds();
     }
 
     return 0;
